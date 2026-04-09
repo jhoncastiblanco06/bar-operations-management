2cd955a08639f7008b7b346547d54a653120379d
Jhon Castiblanco <jhon.castiblanco-c@uniminuto.edu.co>
2026-04-09 00:38:24 -0500

    fix: borrar espacio extra en la API_URL

--- volumes/frontend/utilidades/api.ts
+++ volumes/frontend/utilidades/api.ts
@@ -1,2 +1,7 @@
-// Exportamos la URL para que sea un módulo válido
-export const API_URL = "https://apigg.fljgroup.site ";
+// Detectamos automáticamente si estamos en el servidor o en local
+const esProduccion = process.env.NODE_ENV === "production";
+
+// Asignamos la URL dependiendo del entorno
+export const API_URL = esProduccion
+  ? "https://apigg.fljgroup.site"
+  : "http://localhost:7086";
